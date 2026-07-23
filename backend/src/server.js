5b920a3f7ac655c8b5a69bd7aa562940641210d4
vemuanusha <anushavemu95@gmail.com>
2026-07-23 20:40:44 +0530

    Fix trust proxy

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,7 @@ const rateLimit  = require('express-rate-limit');
 const routes     = require('./routes');
 
 const app  = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 5000;
 
 // ── Middleware ────────────────────────────────────────────────────
